557438d7c04a9e89b92551b841f3ff99f7fa82fb
SonnieCodes <mgathoni.gathoni9@gmail.com>
2024-12-06 04:30:53 +0300

    dom

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const main = document.getElementById('main');
 main.remove();
 
 const newHeader = document.createElement("h1");
-document.body.append(newHeader);
-
 newHeader.id = "victory";
 newHeader.textContent = "Maggie is the champion";
+
+document.body.append(newHeader);
